3b31915a93ab6a9ebc23267bdffd20e91ee6cd42
Ricardo Chong <ricardochong@Ricardos-MacBook-Air.local>
2020-05-07 18:48:11 -0700

    Updates

---
+++ precache-manifest.3c05a6cdca772634e15be5d885640851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40eb25a8d05293381fb89746a155a14e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "317ad60d45e0ae057b11",
+    "url": "/monsters-rolodex/static/css/main.4b304d75.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317ad60d45e0ae057b11",
+    "url": "/monsters-rolodex/static/js/main.8e921d31.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
